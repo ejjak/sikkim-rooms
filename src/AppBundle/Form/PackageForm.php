4faea7e4837b593c9abf8c8b01a036b199bfb4ef
ejjak <ezycreation@gmail.com>
2017-06-05 22:53:24 +0530

    ico

---
+++ src/AppBundle/Form/PackageForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class PackageForm extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name',TextType::class)
+            ->add('phone',TextType::class)
+            ->add('email',EmailType::class)
+            ->add('adult',TextType::class)
+            ->add('minor',TextType::class)
+            ->add('nights',TextType::class)
+            ->add('details',TextareaType::class)
+        ;
+
+    }
+
+}
